ed7dc40064bdf364e3eb3f7f9728105d72178f08
brian <riceb53@gmail.com>
2021-03-23 20:01:26 -0400

    add axios and get data from api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    {{ recipes }}
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function() {
     return {
-      message: "change!!"
+      message: "change!!",
+      recipes: []
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    console.log('in created');
+    this.recipesIndex();
+  },
+  methods: {
+    recipesIndex: function() {
+      console.log('in recipes index');
+      // get data from the api, then show that data to the user
+      axios.get("http://localhost:3000/api/recipes").then(response => {
+        console.log(response.data);
+      });
+    }
+  }
 };
 </script>
 
